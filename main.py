e8aa61417d2d078ab127b8a70225036e457c2994
SeonghunSon@AERO15 <shson1211@tukorea.ac.kr>
2022-10-18 14:41:26 +0900

    draw character

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from pico2d import *
 
 bg = None
+chara = None
 
 class BG:
     def __init__(self):
@@ -20,16 +21,36 @@ class BG:
         elif self.stage == 4:
             self.stage04_image.draw_to_origin(0,0,700, 800)
 
+class Mario:
+    def __init__(self):
+        self.x = 10
+        self.y = 25
+        self.frame = 1
+        self.state = 0
+        self.image = load_image('sprite/mario01.png')
+        self.dying_image = load_image('sprite/mario02.png')
+
+    def draw(self):
+        if self.state == 0:
+            self.image.clip_draw_to_origin(self.frame * 24, 0, 24, 24,  self.x, self.y, 36, 36)
+        elif self.state == 1:
+            self.dying_image.clip_draw_to_origin(self.frame * 24, 0, 24, 24,  self.x, self.y, 36, 36)
+
 def start():
-    global bg
+    global bg, chara
     bg = BG()
+    chara = Mario()
+
+def draw():
+    bg.draw()
+    chara.draw()
 
 
 open_canvas(700, 800)
 start()
 for i in range(0, 4):
     bg.stage += 1
-    bg.draw()
+    draw()
     update_canvas()
     delay(1.0)
 close_canvas()
