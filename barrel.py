74d2e04a0033259b7f541e79790216be57aecb7d
SeonghunSon@AERO15 <shson1211@tukorea.ac.kr>
2022-12-01 22:22:48 +0900

    나무통 클래스 추가

--- barrel.py
+++ barrel.py
@@ -2,77 +2,70 @@ from pico2d import *
 import game_framework
 
 # 이벤트 정의
-TIMER = 0
-event_name = ['TIMER']
+LAND = 0
+event_name = ['LAND']
 key_event_table = {
 
 }
 
-# boss run speed
+# barrel run speed
 PIXEL_PER_METER = (10.0 / 0.2)
-RUN_SPEED_KMPH = 5.0
+RUN_SPEED_KMPH = 6.0
 RUN_SPEED_MPM = (RUN_SPEED_KMPH * 1000.0 / 60.0)
 RUN_SPEED_MPS = (RUN_SPEED_MPM / 60.0)
 RUN_SPEED_PPS = (RUN_SPEED_MPS * PIXEL_PER_METER)
 
-# boss action speed
+# barrel action speed
 TIME_PER_ACTION = 0.5
 ACTION_PER_TIME = 1.0 / TIME_PER_ACTION
 FRAMES_PER_ACTION = 2
 
-
 # 상태 정의
-class IDLE:
+class DROP:
     @staticmethod
     def enter(self,event):
         pass
-    
+
     @staticmethod
     def exit(self,event):
         pass
 
     @staticmethod
     def do(self):
-        self.frame = 1
+        pass
 
     @staticmethod
     def draw(self):
-        self.image.clip_draw_to_origin(int(self.frame) * 50, 0, 50, 36,  self.x, self.y, 100, 72)
+        pass
 
+class ROLL:
+    def enter(self,event):
+        pass
 
-class OPENING:
-    def enter(self, event):
-        self.timer = 400
-
-    def exit(self, event):
+    def exit(self,event):
         pass
 
     def do(self):
-        self.frame = (self.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % 3
-        self.timer -= 1
-        if self.timer == 0:
-            self.add_event(TIMER)
+        pass
 
     def draw(self):
-        self.opening_image.clip_draw_to_origin(int(self.frame) * 50, 0, 50, 36, self.x, self.y, 100, 72)
-
+        pass
 
 next_state = {
-    OPENING: {TIMER: IDLE},
-    IDLE: {}
+    DROP: {LAND: ROLL},
+    ROLL: {}
 }
 
-class Kong:
+class Barrel:
     def __init__(self):
-        self.x, self.y = 20, 538
+        self.x = None
+        self.y = None
         self.frame = 0
-        self.opening_image = load_image('sprite/dk01.png')
-        self.image = load_image('sprite/dk02.png')
-        self.dying_image = load_image('sprite/dk03.png')
-        self.timer = 100
+        self.image = None
+        self.drop_image = None
 
         self.event_que = []
-        self.cur_state = OPENING
+        self.cur_state = DROP
         self.cur_state.enter(self, None)
 
     def update(self):
@@ -84,15 +77,15 @@ class Kong:
                 self.cur_state = next_state[self.cur_state][event]
             except KeyError:
                 print(f'ERROR: STATE {self.cur_state}, EVENT {event_name[event]}')
-            self.cur_state.enter(self, event)
+            self.cur_state.enter(self,event)
 
     def draw(self):
         self.cur_state.draw(self)
 
     def add_event(self, event):
-        self.event_que.insert(0, event)
+        self.event_que.insert(0,event)
 
-    def handle_event(self, event):
+    def handle_event(self,event):
         if (event.type, event.key) in key_event_table:
             key_event = key_event_table[(event.type, event.key)]
             self.add_event(key_event)
