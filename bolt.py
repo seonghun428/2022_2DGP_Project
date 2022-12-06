7f0c267e0e45d01da6494dfbab8cc49ef93e8206
SeonghunSon@AERO15 <shson1211@tukorea.ac.kr>
2022-12-06 17:47:23 +0900

    나사 클래스 추가

---
+++ bolt.py
@@ -0,0 +1,19 @@
+from pico2d import *
+
+class Bolt():
+    def __init__(self,x,y):
+        self.x, self.y = x,y
+        self.image = load_image('sprite/bolt.png')
+    
+    def update(self):
+        pass
+
+    def draw(self):
+        self.image.draw_to_origin(self.x,self.y,25,25)
+        draw_rectangle(*self.get_bb())
+
+    def get_bb(self):
+        return self.x,self.y,self.x + 25, self.y + 25
+
+    def handle_collision(self,other,group):
+        pass
